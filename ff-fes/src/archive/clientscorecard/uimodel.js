7b543f0398d62fa18225daa39b18e2cdf380ba0f
Michael Bakker <michael.bakker@topicus.nl>
2017-03-14 12:51:16 +0100

    Include Solution dependency

--- ff-fes/src/archive/clientscorecard/uimodel.js
+++ ff-fes/src/archive/clientscorecard/uimodel.js
@@ -5,6 +5,7 @@
 //This File should be merged with Node.js,SolutionService.js and not be needed, keep this in mind.
 //introduces a strange level of complexity in the code.
 //rows
+var Solution = require('../fesjs/Solution');
 var indexByRow = {};
 var UIModel = {
     NEW_root_value: {
