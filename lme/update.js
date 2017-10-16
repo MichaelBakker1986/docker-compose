9b33e8a0944a9a300a4719b9ba16a776b78e2598
Michael Bakker <michael.bakker@topicus.nl>
2017-10-16 14:51:55 +0200

    ``

--- lme/update.js
+++ lme/update.js
@@ -7,34 +7,42 @@ var server;
 var busy = false;
 //Ok, ik ga dus de app.js stoppen. een nieuwe staten als child, daarna mezelf stoppen
 app.get('/update', function(req, res) {
-    if (busy) {
-        res.end('budy');
-        return;
-    }
-    busy = true;
-    res.end('succes');
-    console.info('Called update')
-    //pkill -f node
-    exec('git reset --hard origin/master', function(err, response) {
-        if (err) throw err
-        send("<span>Git update</span>")
-        //do update here
-        exec('git pull', function(err, response) {
+    try {
+        if (busy) {
+            res.end('budy');
+            return;
+        }
+        busy = true;
+        res.end('succes');
+        console.info('Called update')
+        //pkill -f node
+        exec('git reset --hard origin/master', function(err, response) {
             if (err) throw err
-            console.info('Excecuted git pull [' + response + ']')
-            send("<span>Restart server</span>");
-            server.close();
-            send("<span>Closed appserver</span>");
-            exec('node update', function(err, response) {
+            send("<span>Git update</span>")
+            //do update here
+            exec('git pull', function(err, response) {
                 if (err) throw err
-                console.info('Killed all node processes [' + response + ']')
-                send("<span>Killed all node processes</span>")
-                busy = false;
-                process.kill();
+                console.info('Excecuted git pull [' + response + ']')
+                send("<span>Restart server</span>");
+                server.close();
+                send("<span>Closed appserver</span>");
+                exec('node update', function(err, response) {
+                    if (err) throw err
+                    console.info('Killed all node processes [' + response + ']')
+                    send("<span>Killed all node processes</span>")
+                    busy = false;
+                    process.kill();
+                })
             })
         })
-    })
+    } catch (err) {
+        busy = false;
+        server = app.listen(port, function() {
+            console.log('Update server running on ' + port + '...');
+        });
+    }
 });
+
 function send(text) {
     request.post({
             url: 'https://topicus.hipchat.com/v2/room/4235024/notification?auth_token=Y9wJuWSkGbOJb5eMiT7GhCtchoQIsjSY9XRF1voW',
