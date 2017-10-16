1992fcf47858ffa092a12a7ecf0a5c7b6f9a82f1
Michael Bakker <michael.bakker@topicus.nl>
2017-10-16 11:36:25 +0200

    ``

---
+++ lme/update.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var port = 8081;
+app.get('/update', function(req, res) {
+    //do update here
+    var exec = require('child_process').exec;
+    exec('git pull', function(err, response) {
+        if (err) throw err
+        console.info('done')
+    })
+    res.end('succes');
+});
+app.listen(port, function() {
+    console.log('Update server running on ' + port + '...');
+});
