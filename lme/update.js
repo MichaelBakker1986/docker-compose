7e25ccd1f07dc3bb19a6fc3d4c63cb7314ff19f3
Michael Bakker <michael.bakker@topicus.nl>
2017-10-16 14:11:34 +0200

    ``

--- lme/update.js
+++ lme/update.js
@@ -2,14 +2,37 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var port = 8081;
+var exec = require('child_process').exec;
 app.get('/update', function(req, res) {
+
+    //pkill -f node
+    exec('git reset --hard origin/master', function(err, response) {
+        if (err) throw err
+        send("<span>Git update</span>")
+    })
+    exec('pkill -f node', function(err, response) {
+        if (err) throw err
+        console.info('Killed all node processes [' + response + ']')
+        send("<span>Killed all node processes</span>")
+    })
+
+    send("<a href='http://blfif-tv-tr03.finance.lab:8080/public/index.html'>DEMO</a>");
+
+    //do update here
+    exec('git pull', function(err, response) {
+        if (err) throw err
+        console.info('Excecuted git pull [' + response + ']')
+        send("<span>Excecuted git pull</span>");
+    })
+    res.end('succes');
+});
+
+function send(text) {
     request.post({
             url: 'https://topicus.hipchat.com/v2/room/4235024/notification?auth_token=Y9wJuWSkGbOJb5eMiT7GhCtchoQIsjSY9XRF1voW',
             json: {
                 "color": "green",
-                "message": "<a href='http://blfif-tv-tr03.finance.lab:8080/public/index.html'>DEMO</a> (yey)",
-                "notify": false,
-                "message_format": "text"
+                "message": text
             }
         },
         function(err, res, body) {
@@ -19,16 +42,8 @@ app.get('/update', function(req, res) {
             console.info(body)
         }
     )
-    //https://topicus.hipchat.com/v2/room/4152124/notification?auth_token=Q5buzgT0rw4jICoa3Hd4xo3IPTp0xURsevNwK6wI
-    //curl -d '{"color":"green","message":"My first notification (yey)","notify":false,"message_format":"text"}' -H 'Content-Type: application/json' https://topicus.hipchat.com/v2/room/4152124/notification?auth_token=Q5buzgT0rw4jICoa3Hd4xo3IPTp0xURsevNwK6wI
-    //do update here
-    var exec = require('child_process').exec;
-    exec('git pull', function(err, response) {
-        if (err) throw err
-        console.info('Excecuted git pull [' + response + ']')
-    })
-    res.end('succes');
-});
+}
+
 app.listen(port, function() {
     console.log('Update server running on ' + port + '...');
 });
