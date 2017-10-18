530370a2f1c1210e17ab8bcaa0c114447fc3b60e
Michael Bakker <michael.bakker@topicus.nl>
2017-10-18 11:47:25 +0200

    Add comment

--- lme/public/stash.js
+++ lme/public/stash.js
@@ -17,7 +17,7 @@ class Stash {
 
     models(branch, path) {
         let command = "git checkout " + branch + " -q && git pull -q && git ls-files-root *." + path;
-        log.info("Do command: [" + command + "]");
+        //log.info("Do command: [" + command + "]");
         return exec(command)
             .then(function(result) {
                 return result.stdout.split('\n');
