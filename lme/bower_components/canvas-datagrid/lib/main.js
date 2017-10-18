884728b59e5c44cac1332e81b1dc1126327d902e
michael.bakker <michael.bakker@blfif-tv-tr03.finance.lab>
2017-10-19 01:42:51 +0200

    changeByDEMO

---
+++ lme/bower_components/canvas-datagrid/lib/main.js
@@ -0,0 +1,60 @@
+/*jslint browser: true, unparam: true, todo: true, evil: true*/
+/*globals Reflect: false, HTMLElement: true, define: true, MutationObserver: false, requestAnimationFrame: false, performance: false, btoa: false*/
+define([
+    './component',
+    './defaults',
+    './draw',
+    './events',
+    './touch',
+    './intf',
+    './contextMenu',
+    './dom',
+    './publicMethods'
+], function context(component) {
+    'use strict';
+    component = component();
+    var modules = Array.prototype.slice.call(arguments);
+    function Grid(args) {
+        args = args || {};
+        var self = {};
+        self.isComponent = args.component === undefined;
+        self.intf = self.isComponent ? eval('Reflect.construct(HTMLElement, [], new.target)') : {};
+        self.args = args;
+        self.createGrid = function grid(args) {
+            args.component = false;
+            return new Grid(args);
+        };
+        modules.forEach(function (module) {
+            module(self);
+        });
+        self.intf.args = self.args;
+        self.intf.init = self.init;
+        if (!self.isComponent) {
+            self.init();
+        }
+        return self.intf;
+    }
+    if (window.HTMLElement) {
+        Grid.prototype = Object.create(window.HTMLElement.prototype);
+    }
+    // export web component
+    if (window.customElements) {
+        Grid.observedAttributes = component.getObservableAttributes();
+        Grid.prototype.disconnectedCallback = function () { this.dispose(); };
+        Grid.prototype.attributeChangedCallback = component.attributeChangedCallback;
+        Grid.prototype.connectedCallback = component.connectedCallback;
+        Grid.prototype.adoptedCallback = component.adoptedCallback;
+        window.customElements.define('canvas-datagrid', Grid);
+    }
+    // export global
+    if (window && !window.canvasDatagrid && !window.require) {
+        window.canvasDatagrid = function (args) { return new Grid(args); };
+    }
+    // export amd loader
+    module.exports = function grid(args) {
+        args = args || {};
+        args.component = false;
+        return new Grid(args);
+    };
+    return module.exports;
+});
