b4f9ff36b682dfed621ab9a40d92ce309cc360bd
Michael Bakker <michael.bakker@topicus.nl>
2017-10-20 13:10:25 +0200

    Add comment

--- ff-fes/exchange_modules/presentation/webexport.js
+++ ff-fes/exchange_modules/presentation/webexport.js
@@ -10,14 +10,49 @@ function WebExport() {
 WebExport.prototype.parse = function(webExport) {
     throw new Error('Not yet supported');
 }
+var counter = 0;
+
+function LMETree(name, workbook) {
+    this.name = name;
+    this.workbook = workbook;
+    this.nodes = [];
+}
+
+LMETree.prototype.addNode = function(node) {
+    var icount = -1;
+    var rval;
+    var workbook = this.workbook;
+    if (!node.hasOwnProperty('value')) {
+        Object.defineProperty(node, 'value', {
+            get: function() {
+                console.info('user get value' + node.rowId)
+                if (icount !== counter) {
+                    icount = counter;
+                    rval = workbook.get(node.rowId, 'value', 0, 0);
+                    if (typeof(rval) === 'object') {
+                        rval = 1;
+                    }
+                }
+                return rval;
+            },
+            set: function(v) {
+                console.info('user set value' + v + ' :' + node.rowId)
+                counter++;
+                workbook.set(node.rowId, v, 'value', 0, 0);
+            }
+        });
+    }
+    this.nodes.push(node);
+}
+
 WebExport.prototype.deParse = function(rowId, workbook) {
     var modelName = workbook.getSolutionName();
     var rootNode = workbook.getRootSolutionProperty(modelName);
-
-}
-
-function createTreeNode(tree, nodeId, displayAs) {
-    return tree.createNode(nodeId);
+    var lmeTree = new LMETree(modelName, workbook);
+    workbook.visitProperties(rootNode, function(node) {
+        lmeTree.addNode(node)
+    })
+    return lmeTree;
 }
 
 SolutionFacade.addParser(new WebExport())
