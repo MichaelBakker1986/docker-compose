2ae983755aae962e1b9ada6bc393ab60bb266455
Michael Bakker <michael.bakker@topicus.nl>
2017-11-01 17:39:56 +0100

    dynamic swagger call

--- lme-api/ff-fes-app.js
+++ lme-api/ff-fes-app.js
@@ -8,6 +8,7 @@ var log = require('ff-log');
 var fs = require('fs')
 var Promise = require('promise')
 var bodyParser = require('body-parser');
+const hostname = require('os').hostname();
 app.use(bodyParser.json()); // To support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // To support URL-encoded bodies
     extended: true,
@@ -23,10 +24,14 @@ app.use('/docs', swaggerUi({
 }));
 // swagger definition
 app.get('/api-docs', function(req, res) {
-    res.json(require(__dirname + '/swaggerDef.json'))
+    let swaggerData = require(__dirname + '/swaggerDef.json');
+    require('dns').lookup(hostname, (err, add, fam) => {
+        swaggerData.host = add + ':' + port;
+        res.json(swaggerData)
+    })
 });
 app.listen(port, function() {
-    require('dns').lookup(require('os').hostname(), (err, add, fam) => {
+    require('dns').lookup(hostname, (err, add, fam) => {
         let domain = 'http://' + add + ':' + port + '/';
         console.info('<a href="' + domain + 'docs">Swagger API docs</a><span> up.</span></br>\n');
     })
