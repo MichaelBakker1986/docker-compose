40475d8165b656adf7789c6ca9a4cdd8da16e11c
Michael Bakker <michael.bakker@topicus.nl>
2017-11-02 15:59:31 +0100

    Implement client-valuestore

--- lme-api/api/store.js
+++ lme-api/api/store.js
@@ -1,7 +1,23 @@
+/**
+ * While calculating in front-end.
+ * There is a need to store/retrieve values entered by the client
+ */
 const MatrixStore = require('../MatrixStore').MatrixStore;
 module.exports.setup = function(app) {
     var ds = new MatrixStore();
-    app.get('/id/:id/', function(req, res) {
+    /**
+     * Retrieve entered values supplied by the client
+     */
+    app.get('/id/:id', function(req, res) {
         res.json(ds.getOrCreate(req.params.id))
+    });
+    /**
+     * Store entered values supplied by the client
+     */
+    app.post('/id/:id', function(req, res) {
+        //resolve all entered values
+        res.json({
+            saveToken: 1234
+        })
     });
 };
