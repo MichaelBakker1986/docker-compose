473a6adcdc8b0f5878141cc6709c85683ad94cc7
Michael Bakker <michael.bakker@topicus.nl>
2017-11-02 10:36:54 +0100

    Convert simtuple into realtuple

--- lme-model-tests/V05/V05SimtupleToRealTuple.js
+++ lme-model-tests/V05/V05SimtupleToRealTuple.js
@@ -1,38 +1,50 @@
+/**
+ * Convert generated tuples into tuples.
+ */
+const figureName = 'V05';
 const [assert, importModel, LME, log, readFileSync, writeFileSync] = require('../PRICING/ModelFacade')
 
 var linecounter = 0;
 var simtuples = []
 var bracketOpen = false;
 var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream(__dirname + '/../V05/V05.FFL')
+    input: require('fs').createReadStream(__dirname + '/' + figureName + '.FFL')
 });
-
-/*var LineReader = require('linereader');
-var lineReader = new LineReader(__dirname + '/../V05/V05.FFL');*/
-
+var outputFFL = "";
+let bracketcounter = 0;
 lineReader.on('line', function(line) {
     linecounter++;
-
-    if (line.match(/(variable|tuple) (\-|\+|\=){0,1}\w+_ti[0-9]{2}[2-9]{1}/gmi)) {
+    if (line.match(/(variable|tuple) (\-|\+|\=){0,1}\w+_ti([0-9]{2}[2-9]{1}|[0-9]{1}1[0-9]{1})/gmi)) {
         bracketOpen = true;
         simtuples.push({
             ln: linecounter
         })
-        //  console.log('Line from file:', line);
+    }
+    else if (bracketOpen && line.match(/^\s*{\s*$/gmi)) {
+        bracketcounter++;
     }
     else if (bracketOpen && line.match(/^\s*}\s*$/gmi)) {
-        bracketOpen = false;
+        bracketcounter--;
+        if (bracketcounter == 0) {
+            bracketOpen = false;
+        }
         simtuples[simtuples.length - 1].lnend = linecounter
-        //   console.log('Line from file:', line);
+    }
+    else if (!bracketOpen) {
+        outputFFL += line + '\n';
     }
 });
 lineReader.on('close', function() {
-    console.info(simtuples)
+    //var linesAsArray = data.split('\n').slice(1).join('\n');
+    log.info(simtuples)
+    outputFFL = outputFFL.replace(/_ti[0-9]{3}/gmi, '')//just remove Tuple completely
+    writeFileSync(__dirname + '/' + figureName + '_realtuple.ffl', outputFFL)
+    defaultCase();
 });
 
 function defaultCase() {
-    let V05ffl = readFileSync(__dirname + '/../V05/V05.FFL');
-//some case-bugfixes
+    let V05ffl = readFileSync(__dirname + '/' + figureName + '_realtuple.ffl');
+    //some case-bugfixes
     V05ffl = V05ffl.replace(/amount/gmi, 'Amount')
     V05ffl = V05ffl.replace(/Bookvalue/gmi, 'BookValue')
     V05ffl = V05ffl.replace(/GoodWill/gmi, 'GoodWill')
@@ -49,6 +61,5 @@ function defaultCase() {
     HiddenVars.value;
 
     let fixProblemsInImportedSolution = LME.lme.fixProblemsInImportedSolution();
-    console.info(fixProblemsInImportedSolution)
-
+    log.info(fixProblemsInImportedSolution)
 }
