493c32650bae6d6128cf777b17e872b79ef066aa
Michael Bakker <michael.bakker@topicus.nl>
2017-11-06 14:13:57 +0100

    Add hint, choices Add tests

--- lme-model-tests/MVO/MVOeditorShow.js
+++ lme-model-tests/MVO/MVOeditorShow.js
@@ -16,7 +16,8 @@ MVOeditorShow.prototype.parse = function(input) {
     var parser = require('../../ff-fes/exchange_modules/ffl/FflToJsonConverter')
     var JSVisitor = require('../../ff-fes/fesjs/JSVisitor')
     var result = parser.parseFFL(input)
-    let objectModel = result['model MVO uses BaseModel'][''];
+    var solutionName = findSolutionNameFromFFLFile(result);
+    let objectModel = result['model ' + solutionName + ' uses BaseModel'][''];
     var output = [];
     JSVisitor.travelOne(objectModel, null, function(keyArg, node, context) {
         if (keyArg === null) {
@@ -25,6 +26,8 @@ MVOeditorShow.prototype.parse = function(input) {
             var tupleDefiniton = keyArg.startsWith('tuple ');
             if ((keyArg.startsWith('variable ') || tupleDefiniton)) {
                 var nodeName = stripVariableOrtuple(keyArg, node);
+
+
                 var parent = JSVisitor.findPredicate(node, StartWithVariableOrTuplePredicate)
                 var parentId = (parent === undefined ? undefined : stripVariableOrtuple(parent._name, parent));
                 if (tupleDefiniton) {
@@ -37,9 +40,13 @@ MVOeditorShow.prototype.parse = function(input) {
                 var hint = node.hint || false
                 if (formula) {
                     var spaces = [];
-                    let total = getdepth(node, 0) + " " + nodeName + " " + (locked ? 'locked ' : ' ') + (visible ? 'visible ' : ' ') + (required ? 'required ' : ' ');
-                    spaces.length = 70 - total.length;
-                    output.push(total + spaces.join(' ') + '=' + node.formula);
+                    let depth = getdepth(node, 0);
+                    let total = (node.modifier || '') + nodeName + " " + (locked ? 'locked ' : ' ') + (visible ? 'visible ' : ' ') + (required ? 'required ' : ' ');
+                    spaces.length = (70 - total.length) - depth;
+                    let prefix = [];
+                    prefix.length = depth;
+
+                    output.push(depth + " " + prefix.join(' ') + total + spaces.join(' ') + '=' + node.formula);
                     //output.push('value:' + spaces.join(' ') + "=" + node.formula)
                     if (hint) {
                         // output.push(' hint:' + spaces.join(' ') + "=" + node.hint)
@@ -95,6 +102,16 @@ MVOeditorShow.prototype.parse = function(input) {
 
     return output.join('\n');
 }
+
+function findSolutionNameFromFFLFile(json) {
+    for (var key in json) {
+        if (key.toLowerCase().startsWith('model ')) {
+            return key.split(' ')[1].toUpperCase();
+        }
+    }
+    return undefined;
+}
+
 //console.info(new MVOeditorShow().parse(require('fs').readFileSync(__dirname + '/MVO.ffl', 'utf8')))
 exports.MVOeditorShow = new MVOeditorShow();
 
