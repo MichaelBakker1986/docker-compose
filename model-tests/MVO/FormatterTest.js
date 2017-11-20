dfe2f0558f38f66cab1c72bf86df57234e70a592
Michael Bakker <michael.bakker@topicus.nl>
2017-11-20 08:47:18 +0100

    add formatter test

--- model-tests/MVO/FormatterTest.js
+++ model-tests/MVO/FormatterTest.js
@@ -1,8 +1,7 @@
 const now = require('performance-now')
-
 var Formatter = require('../plugins/FFLFormatter').LexialParser
-let fflFile = require('fs').readFileSync('../../ff-ssh-git/resources/V05.ffl', 'utf8');
+let fflFile = require('fs').readFileSync(__dirname + '/MVO.ffl', 'utf8');
 var start = now()
 let parse = Formatter.parse(fflFile);
-console.info(Object.keys(parse.vars()).length)
-console.info((now() - start))
+//the MVO.ffl is already formatted, the result is the same when formatting twice.
+require('assert').ok(parse, fflFile)
