ee6d591542db0b3504189fff21a83052cd4fa250
Bart Schapendonk <bart.schapendonk@topicus.nl>
2017-12-13 13:48:24 +0100

    gulp gaat ook niet werken

--- lme-ksp-api-as/gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const zip = require('gulp-zip');
-
-gulp.task('default', () =>
-    gulp.src(['index.js', 'node_modules/**'], { follow: true })
-        .pipe(zip('archive.zip'))
-        .pipe(gulp.dest('dist'))
-);
