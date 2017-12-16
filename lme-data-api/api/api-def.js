0039461d43beaa1dbe38acae3a186b0dc2914fd1
Michael Bakker <michael.bakker@topicus.nl>
2017-12-16 12:44:02 +0100

    update swagger host

--- lme-data-api/api/api-def.js
+++ lme-data-api/api/api-def.js
@@ -1,18 +1,14 @@
 const swaggerUi = require('swaggerize-ui');
-const hostname = require('os').hostname();
 
 module.exports.setup = function(app) {
-    //swagger
     app.use('/docs', swaggerUi({
-        docs: '/api-docs?abc=1' // from the express route above.
+        docs: '/api-docs' // from the express route above.
     }));
-    // swagger definition
     app.get('/api-docs', function(req, res) {
-        let port = app.get('port');
+        const port = app.get('port');
+        const host = app.get('host');
         let swaggerData = require(__dirname + '/swaggerDef.json');
-        require('dns').lookup(hostname, (err, add, fam) => {
-            swaggerData.host = add + ':' + port;
-            res.json(swaggerData)
-        })
+        swaggerData.host = host + ':' + port;
+        res.json(swaggerData)
     });
 };
